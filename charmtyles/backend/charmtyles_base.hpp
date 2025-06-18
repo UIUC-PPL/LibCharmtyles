38ec5e93e0543f7e3f3f081a1531a4365c0f3e46
Advait Tahilyani <advaitt3@illinois.edu>
2025-06-18 15:13:41 -0400

    Fixed loop unrolling

--- charmtyles/backend/charmtyles_base.hpp
+++ charmtyles/backend/charmtyles_base.hpp
@@ -495,25 +495,25 @@ private:
 
             return;
         case ct::util::Operation::unary_expr:
-            total_size = mat_map[node_id].rows();
-            unrolled_size = mat_map[node_id].rows() / 4;
+            total_size = mat_map[node_id].cols();
+            unrolled_size = mat_map[node_id].cols() / 4;
             remainder_start = unrolled_size * 4;
-            for (std::size_t i = 0; i != remainder_start; i += 4)
+            for (std::size_t i = 0; i != mat_map[node_id].rows(); i++)
             {
-                for (std::size_t j = 0; j != mat_map[node_id].cols(); ++j)
+                for (std::size_t j = 0; j != remainder_start; j += 4)
                 {
                     unary_expr->operator()(i, j, mat_map[node_id](i, j));
                     unary_expr->operator()(
-                        i + 1, j, mat_map[node_id](i + 1, j));
+                        i, j + 1, mat_map[node_id](i, j + 1));
                     unary_expr->operator()(
-                        i + 2, j, mat_map[node_id](i + 2, j));
+                        i, j + 2, mat_map[node_id](i, j + 2));
                     unary_expr->operator()(
-                        i + 3, j, mat_map[node_id](i + 3, j));
+                        i, j + 3, mat_map[node_id](i, j + 3));
                 }
             }
-            for (std::size_t i = remainder_start; i != total_size; ++i)
+            for (std::size_t i = 0; i != mat_map[node_id].rows(); ++i)
             {
-                for (std::size_t j = 0; j != mat_map[node_id].cols(); ++j)
+                for (std::size_t j = remainder_start; j != total_size; ++j)
                 {
                     unary_expr->operator()(i, j, mat_map[node_id](i, j));
                 }
