6e6264212acff48030e2054296c9cb3c8dc5d497
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-09-24 00:45:03 +0530

    success

--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -9,6 +9,50 @@ class Main : public CBase_Main
 public:
     Main(CkArgMsg* msg)
     {
+        {
+              Kokkos::initialize();
+  {
+    Kokkos::View<int*> a("v", 5);
+    Kokkos::View<int*> b("v", 5);
+    Kokkos::View<int*> c("v", 5);
+
+    Kokkos::parallel_for("fill", 5, KOKKOS_LAMBDA(int i) { a(i) = 2; });
+    Kokkos::parallel_for("fill", 5, KOKKOS_LAMBDA(int i) { b(i) = 3; });
+    Kokkos::parallel_for("fill", 5, KOKKOS_LAMBDA(int i) { c(i) = a(i) + b(i); });
+    auto chost = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), c);
+    KOKKOS_ASSERT(chost(0) == 5);
+
+    Kokkos::View<int**> a_("a", 5, 5);
+    Kokkos::View<int**> b_("b", 5, 5);
+    Kokkos::View<int**> c_("c", 5, 5);
+
+    Kokkos::parallel_for(
+      "fill_a",
+      Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {5,5}),
+      KOKKOS_LAMBDA(int i, int j) {
+        a_(i, j) = 2;
+      });
+
+    Kokkos::parallel_for(
+      "fill_b",
+      Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {5,5}),
+      KOKKOS_LAMBDA(int i, int j) {
+        b_(i, j) = 3;
+      });
+
+    Kokkos::parallel_for(
+      "compute_c",
+      Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {5,5}),
+      KOKKOS_LAMBDA(int i, int j) {
+        c_(i, j) = a_(i, j) * b_(i, j);
+      });
+
+    auto c_host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), c_);
+    KOKKOS_ASSERT(c_host(0, 0) == 6);
+  }
+  Kokkos::printf("Goodbye World\n");
+  Kokkos::finalize();
+        }
         int num_pes = 6;
         if (msg->argc > 1)
             num_pes = atoi(msg->argv[1]);
