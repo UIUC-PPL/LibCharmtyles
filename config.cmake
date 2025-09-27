1f82f03aced6c88d41434c74489056675f2ddc87
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-09-27 11:04:03 +0530

    cmake build switch

--- config.cmake
+++ config.cmake
@@ -3,6 +3,6 @@ set(BASE_DIR "/home/shogo/master/Kale/charmTylesUpstream/LibCharmtyles")
 set(EIGEN_DIR "/usr/include/eigen3")
 
 set(CHARMC "${CHARM_DIR}/bin/charmc")
-set(OPTS "-c++-option -std=c++17 -O3 -march=native -DNDEBUG")
+set(OPTS "-std=c++17 -O3 -march=native -DNDEBUG")
 set(LD_OPTS "")
 set(INCS "-I${BASE_DIR}")
