1b28ef96873849bb0ca7b7d710d080d916025a84
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-11-27 10:29:04 +0530

    fixes

---
+++ gpu_env.sh
@@ -0,0 +1,6 @@
+module load cuda/12.4.0
+module load cmake/3.27.9
+module load eigen
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:path/to/LibCharmtyles/kokkos-kernels/install/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:path/to/LibCharmtyles/kokkos/install/lib64
